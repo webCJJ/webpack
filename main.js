6cb4a8a11ff097c54857a12218e3a6cd05c08668
cjj <1228424102@qq.com>
2017-09-05 17:30:06 +0800

    webpack hello world

---
+++ main.js
@@ -0,0 +1,5 @@
+/**
+ * Created by cjj on 2017/9/5.
+ */
+document.write("<h1>hello world</h1>");
+console.log("Ok");
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by cjj on 2017/9/5.
+ */
+module.exports = {
+    entry : './main.js',
+    output:{
+        filename:'bundle.js'
+    }
+}
